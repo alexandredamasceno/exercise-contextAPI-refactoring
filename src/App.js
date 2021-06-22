12cd8a35d8ab3215b11a9363ae8258d3d718469e
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-06-22 00:34:37 -0300

    Finish exercise one

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './context/MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+    }
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(cor, side) {
+    this.setState({
+      cars: {
+        [cor]: side,
+      }
+    })
+  }
+
+  render() {
+    const myObj = {
+      ...this.state,
+      moveCar: this.moveCar
+    }
+
+    return (
+      <MyContext.Provider value={ myObj }>
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
